4a405a6f342ee8a89579ca6da7518aaa5c72482f
Joniel44 <joniel_25@hotmail.com>
2025-08-13 23:40:17 +0000

    criando

---
+++ batalhanavalnovato.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+
+  printf("...Bem Vindo a Batalha Naval... \n\n");
+
+  char linha[10]= {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+  int coluna[10]= {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int tabuleiro[10][10]; 
+  
+  for (int i = 0; i < 10; i++)
+  {
+    printf(" %c", linha[i]);
+
+  }   
+   
+    for (int j = 0; j < 10; j++)
+  {
+    printf("\n%d ", coluna[j]);
+  } 
+     
+     
+
+
+
+
+  
+     
+  return 0;
+    
+        
+}

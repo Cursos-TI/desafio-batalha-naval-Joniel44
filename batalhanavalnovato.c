4aace466a70399fee5eee868b75b986893645c7d
Joniel44 <joniel_25@hotmail.com>
2025-08-14 00:41:07 +0000

    criando1.2

--- batalhanavalnovato.c
+++ batalhanavalnovato.c
@@ -1,29 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(){
 
-  printf("...Bem Vindo a Batalha Naval... \n\n");
+  printf("\n...Bem Vindo a Batalha Naval... \n\n");
 
   char linha[10]= {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-  int coluna[10]= {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   int tabuleiro[10][10]; 
   
-  for (int i = 0; i < 10; i++)
-  {
-    printf(" %c", linha[i]);
+   // Inicializa o tabuleiro com 0 (Representando a água)
 
-  }   
+ for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++) {
+       tabuleiro[i][j] = 0; // Inicializa o tabuleiro com 0
+      }  
+  }
+     // Imprime o cabeçalho das colunas
+  printf("   ");     // Espaço para alinhar com as letras das linhas   
+  for (int j = 1; j <= 10; j++) {
+    printf("%d ", j);
+  }
+     printf(" \n");
+
+    // Imprime tabuleiro com letras e números
+   for (int i = 0; i < 10; i++)  {
+    printf("%2c ", linha[i]);          // Imprime a letra da linha
+      for (int j = 0; j < 10; j++) {
+        printf("%d ", tabuleiro[i][j]);     // imprime os valores da matrizes
+      }
+      printf(" \n");
+   }
    
-    for (int j = 0; j < 10; j++)
-  {
-    printf("\n%d ", coluna[j]);
-  } 
-     
-     
-
-
-
 
   
      

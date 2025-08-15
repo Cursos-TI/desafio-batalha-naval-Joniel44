1388fe7b4651cd7385d798f763e9b76bcfcc2c9f
Joniel44 <joniel_25@hotmail.com>
2025-08-15 22:12:43 +0000

    adventure1.2

--- batalhanavalnovato.c
+++ batalhanavalnovato.c
@@ -11,21 +11,20 @@ int main(){
 
  for (int i = 0; i < 10; i++) {
     for (int j = 0; j < 10; j++) {
-       tabuleiro[i][j] = 0;        // Inicializa o tabuleiro com 0
+       tabuleiro[i][j] = 0;                 // Inicializa o tabuleiro com 0
       }  
   }
-     // Define os navios na posição horizontal e vertical
+     // Define a posição dos navios no diagonal, horizontal e vertical
 
-   tabuleiro[3][3] = 3; tabuleiro[3][4] = 3; tabuleiro[3][5] = 3;
-   tabuleiro[4][0] = 3; tabuleiro[5][0] = 3; tabuleiro[6][0] = 3;
-   tabuleiro[5][6] = 3; tabuleiro[6][5] = 3; tabuleiro[4][7] = 3;  
-   tabuleiro[6][6] = 3; tabuleiro[7][7] = 3; tabuleiro[8][8] = 3;
+    tabuleiro[0][3] = 3; tabuleiro[0][4] = 3; tabuleiro[0][5] = 3;        // Navio horizontal
+    tabuleiro[2][0] = 3; tabuleiro[3][0] = 3; tabuleiro[4][0] = 3;        // Navio vertical
+    tabuleiro[1][8] = 3; tabuleiro[2][7] = 3; tabuleiro[3][6] = 3;        // Navio diagonal  
+    tabuleiro[6][6] = 3; tabuleiro[7][7] = 3; tabuleiro[8][8] = 3;       // Navio diagonal
 
+  // Imprime o cabeçalho das colunas
 
-     // Imprime o cabeçalho das colunas
-
-  printf("   ");         // Espaço para alinhar com as letras das linhas   
-  for (int j = 1; j <= 10; j++) {
+  printf("   ");                   // Espaço para alinhar com as letras das linhas   
+  for (int j = 0; j <= 10; j++) {
     printf("%d ", j);
   }
      printf(" \n");
@@ -33,7 +32,7 @@ int main(){
     // Imprime tabuleiro com letras e números
 
    for (int i = 0; i < 10; i++)  {
-    printf("%2c ", linha[i]);               // Imprime a letra da linha
+    printf("%2c ", linha[i]);          // Imprime a letra da linha
       for (int j = 0; j < 10; j++) {
         printf("%d ", tabuleiro[i][j]);     // imprime os valores da matrizes
       }

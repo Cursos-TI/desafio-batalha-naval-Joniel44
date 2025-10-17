f8ea3cd506ef2a970dd81462927f5e37b0497bfb
Joniel44 <joniel_25@hotmail.com>
2025-10-17 13:23:39 +0000

    finalizando.c

--- batalhanavalnovato.c
+++ batalhanavalnovato.c
@@ -1,48 +1,99 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define linhas 10
+#define colunas 10
+#define agua 0
+
+// Função genérica para desenhar uma forma no tabuleiro
+
+void desenharForma(int tabuleiro[linhas][colunas], int forma[5][5], int tam, int linhaInicio, int colunaInicio, int valor) {
+    for (int i = 0; i < tam; i++) {
+        for (int j = 0; j < tam; j++) {
+            if (forma[i][j] != 0) { // só desenha se for parte da forma
+                int x = linhaInicio + i;
+                int y = colunaInicio + j;
+                if (x >= 0 && x < linhas && y >= 0 && y < colunas) {
+                    tabuleiro[x][y] = valor;
+                }
+            }
+        }
+    }
+}
+
 
 int main(){
 
-  printf("\n...Bem Vindo a Batalha Naval... \n\n");
+char linha[linhas] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}; // Linhas com as consoantes
+int tabuleiro[linhas][colunas];
 
-  char linha[10]= {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-  int tabuleiro[10][10]; 
-  
-   // Inicializa o tabuleiro com 0 (Representando a água)
+printf("\n***Batalha Naval***\n\n");
 
- for (int i = 0; i < 10; i++) {
-    for (int j = 0; j < 10; j++) {
-       tabuleiro[i][j] = 0;                 // Inicializa o tabuleiro com 0
-      }  
-  }
-     // Define a posição dos navios no diagonal, horizontal e vertical
-
-    tabuleiro[0][3] = 3; tabuleiro[0][4] = 3; tabuleiro[0][5] = 3;        // Navio horizontal
-    tabuleiro[2][0] = 3; tabuleiro[3][0] = 3; tabuleiro[4][0] = 3;        // Navio vertical
-    tabuleiro[1][8] = 3; tabuleiro[2][7] = 3; tabuleiro[3][6] = 3;        // Navio diagonal  
-    tabuleiro[6][6] = 3; tabuleiro[7][7] = 3; tabuleiro[8][8] = 3;       // Navio diagonal
-
-  // Imprime o cabeçalho das colunas
-
-  printf("   ");                   // Espaço para alinhar com as letras das linhas   
-  for (int j = 1; j <= 10; j++) {
-    printf("%d ", j);
-  }
-     printf(" \n");
-
-    // Imprime tabuleiro com letras e números
-
-   for (int i = 0; i < 10; i++)  {
-    printf("%2c ", linha[i]);          // Imprime a letra da linha
-      for (int j = 0; j < 10; j++) {
-        printf("%d ", tabuleiro[i][j]);     // imprime os valores da matrizes
-      }
-      printf(" \n");
-   }
-   
-
-  
+for(int i = 0; i < linhas; i++) {
+   for(int j = 0; j < colunas; j++) {
+     tabuleiro[i][j] = agua;
+     }
+     }
      
-  return 0;
+     // Navios
+
+     tabuleiro[0][1] = 3; tabuleiro[0][2] = 3; tabuleiro[0][3] = 3;
+     tabuleiro[1][5] = 3; tabuleiro[2][5] = 3; tabuleiro[3][5] = 3;
+     tabuleiro[3][7] = 3; tabuleiro[4][6] = 3; tabuleiro[5][5] = 3;
+     tabuleiro[7][5] = 3; tabuleiro[8][6] = 3; tabuleiro[9][7] = 3;
     
-        
+     // Máscara do Cone (5x5 no máximo)
+    int cone[5][5] = {
+        {0,0,1,0,0},
+        {0,1,1,1,0},
+        {1,1,1,1,1},
+        {0,0,0,0,0},
+        {0,0,0,0,0}
+    };
+
+    // Máscara do Octaedro (5x5)
+    int octaedro[5][5] = {
+        {0,0,1,0,0},
+        {0,1,1,1,0},
+        {0,0,1,0,0},
+        {0,0,0,0,0},
+        {0,0,0,0,0}
+    };
+
+    // Máscara da Cruz (5x5)
+    int cruz[5][5] = {
+        {0,0,1,0,0},
+        {1,1,1,1,1},
+        {0,0,1,0,0},
+        {0,0,0,0,0},
+        {0,0,0,0,0}
+    };
+
+     // Desenha as formas no tabuleiro
+    desenharForma(tabuleiro, cone, 5, 3, 0, 5);      // cone na posição (3,0) valor 5
+    desenharForma(tabuleiro, octaedro, 5, 0, 6, 5);  // octaedro na posição (0,6) valor 5
+    desenharForma(tabuleiro, cruz, 5, 6, 0, 7);      // cruz na posição (6,0) valor 7
+
+
+
+     printf("   ");
+     for (int j = 1; j <= colunas; j++)  {
+     printf("%d ", j);
+     }
+     printf("\n");
+
+     for (int i = 0; i < linhas; i++)
+     {
+     printf("%2c ", linha[i]);
+     for (int j = 0; j < colunas; j++) {
+     printf("%d ", tabuleiro[i][j]);
+     }
+     printf("\n");
+     }
+    
+  
+      
+    
+     return 0;
+
 }
